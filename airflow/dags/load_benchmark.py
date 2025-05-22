d780bb33fde583d2b7beee84fbad034387215ef4
TrieuBui1012 <trieubqt1012@gmail.com>
2025-05-23 06:16:03 +0700

    updated load-benchmark dag

--- airflow/dags/load_benchmark.py
+++ airflow/dags/load_benchmark.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta
 from airflow import DAG
 from airflow.exceptions import AirflowException
 from airflow.utils import yaml
