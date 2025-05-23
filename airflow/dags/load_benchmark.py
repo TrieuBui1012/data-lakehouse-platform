87c94c2ccfb068708c9967cafd220e566d42a3bc
TrieuBui1012 <trieubqt1012@gmail.com>
2025-05-23 12:37:32 +0700

    updated load-benchmark dag

--- airflow/dags/load_benchmark.py
+++ airflow/dags/load_benchmark.py
@@ -14,7 +14,7 @@ with DAG(
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Bangkok"),
     catchup=False,
     dagrun_timeout=timedelta(minutes=60),
-    tags=["example"],
+    tags=["load", "benchmark"],
     params={},
 ) as dag:
 
